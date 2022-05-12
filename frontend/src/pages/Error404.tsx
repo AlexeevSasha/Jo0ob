0a0ce7f1635937e1c3726befaad03b41d9dee727
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-12 11:50:38 +0300

    404

---
+++ frontend/src/pages/Error404.tsx
@@ -0,0 +1,44 @@
+import {FC} from "react";
+import {Link} from "react-router-dom";
+import logo from '../assets/images/error.svg'
+import {Typography} from 'antd';
+import styled from 'styled-components'
+
+const {Title, Paragraph} = Typography;
+
+export const Error404: FC = () => {
+    return (
+        <Flex>
+            <img src={logo} alt="error"/>
+            <Title level={1}>Ohh! page not found</Title>
+            <Paragraph>We can`t seem to find the page you`re looking for</Paragraph>
+            <Link to='/dashboard'>back home</Link>
+        </Flex>
+    )
+}
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+  height: 100vh;
+  padding: 20px;
+  text-align: center;
+  & > img {
+    max-width: 500px;
+    width: 100%;
+    margin-bottom: 40px;
+  }
+  & > h1 {
+    color: inherit;
+    font-weight: 700;
+    font-size: 50px;
+  }
+
+  @media ${({theme}) => theme.media._768} {
+    & > h1 {
+      font-size: 25px;
+    }
+  }
+`
