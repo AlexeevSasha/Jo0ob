980d45ca3d1dc3e037d4e5f8e38cb2f993095b4e
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-13 10:12:01 +0300

    init store provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
 import {ThemeProvider} from "styled-components";
 import {theme} from './assets/css/theme'
+import {store} from './redux/store'
 import App from './App';
 
 import 'antd/dist/antd.less'
@@ -13,9 +15,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
+        <Provider store={store}>
         <ThemeProvider theme={theme}>
             <App/>
         </ThemeProvider>
+        </Provider>
     </React.StrictMode>
 );
 
