9f81c4fb1275da3dd68a579b5af40c02b64671c9
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-13 10:11:44 +0300

    auth provider router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Dashboard, Main, Error404, Login, Register} from "./pages";
+import {AuthProvider, UserProvider} from "./hooks/AuthProvider";
 
 
 
@@ -7,10 +8,14 @@ function App() {
   return (
      <BrowserRouter>
          <Routes>
-             <Route path='/login' element={<Login/>}/>
-             <Route path='/register' element={<Register/>}/>
-             <Route path='/' element={<Main/>}/>
-             <Route path='/dashboard' element={<Dashboard/>}/>
+             <Route  element={<UserProvider/>}>
+                 <Route path='/login' element={<Login/>}/>
+                 <Route path='/register' element={<Register/>}/>
+                 <Route path='/' element={<Main/>}/>
+             </Route>
+             <Route  element={<AuthProvider/>}>
+                 <Route path='/dashboard' element={<Dashboard/>}/>
+             </Route>
              <Route path='*' element={<Error404/>}/>
          </Routes>
      </BrowserRouter>
