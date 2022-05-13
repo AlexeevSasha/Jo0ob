3e1cbd5262aac6a93caf5085edda22f9ba4be963
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-13 10:12:21 +0300

    auth and user provider

---
+++ frontend/src/hooks/AuthProvider.tsx
@@ -0,0 +1,25 @@
+import {FC, useEffect} from "react";
+import {Outlet} from "react-router-dom";
+import {useNavigate, Navigate} from "react-router-dom";
+import {useAppSelector} from "../redux/reduxType";
+
+
+
+export const AuthProvider: FC= () => {
+    const { user } = useAppSelector(state => state.auth)
+
+    if (!user) {
+        return <Navigate to="/" replace/>;
+    }
+
+    return <Outlet/>
+}
+
+export const UserProvider: FC= () => {
+    const { user } = useAppSelector(state => state.auth)
+
+    if (user) {
+        return <Navigate to="/dashboard" replace/>;
+    }
+    return <Outlet/>
+}
