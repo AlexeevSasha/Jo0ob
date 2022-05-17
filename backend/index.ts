e39cde03be0601ddb6cd60ef5b7b1fbbf41d0d91
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-17 09:22:39 +0300

    middleware cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,23 +1,31 @@
+import cors from 'cors'
 import express from 'express';
+import morgan from "morgan";
 import {json} from 'body-parser';
 import dotenv from 'dotenv'
+import 'express-async-errors'
 import {connectDB} from "./db/connect";
 import authRouter from "./routes/authRouter";
 import jobsRouter from "./routes/jobsRouter";
 import {notFoundMiddleware} from "./middleware/not-found";
-import {errorHandlerMiddleware} from './middleware/error-handler'
+import {errorHandlerMiddleware} from './middleware/error-handler';
 
 
 dotenv.config()
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome!')
-})
+
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+}
+
 
 app.use('/api/auth', authRouter)
 app.use('/api/jobs', jobsRouter)
@@ -26,10 +34,10 @@ app.use(errorHandlerMiddleware)
 app.use(notFoundMiddleware)
 
 
-const start = async () => {
+const start = async () : Promise<void> => {
     try {
         await connectDB(process.env.MONGO_URL ?? '')
-        app.listen(PORT, (): void => {
+        app.listen(PORT,  (): void => {
             console.log('Server running....');
         });
     } catch (error) {
