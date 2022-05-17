3a2028dd11421ba18d6af923e6e74ab5573def79
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-17 09:36:42 +0300

    auth extra-reducer

--- frontend/src/redux/slices/auth.ts
+++ frontend/src/redux/slices/auth.ts
@@ -1,33 +1,62 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {loginThunk, registerThunk} from '../thunk/auth'
 import {STATUS} from "../reduxType";
+import {addUserLocalStorage} from "../../hooks/localStorage";
+import {IUser} from "../../api/auth/authDto";
 
-interface IUser {
-    name: string;
-    email: string;
-}
 
 interface IAuth  {
-    user: string | null;
-    status: STATUS
+    user: IUser | null;
+    token: string
+    status: STATUS;
 }
 
 const initialState : IAuth = {
     user:  JSON.parse(`${localStorage.getItem("user")}`) || null,
-    status: STATUS.NEVER
+    token: '',
+    status: STATUS.NEVER,
 }
 
 export const auth = createSlice({
     name:'auth',
     initialState,
     reducers: {
-        addUserTest(state, action: PayloadAction<string>) {
-            localStorage.setItem('user', JSON.stringify(action.payload))
-            state.user = action.payload;
-            state.status = STATUS.LOADED
-        }
+
     },
-    extraReducers: {}
+    extraReducers: (builder => {
+        //register
+        builder.addCase(registerThunk.pending, (state) => {
+            state.status = STATUS.LOADING
+            state.user = null;
+        })
+        builder.addCase(registerThunk.fulfilled, (state, {payload}) => {
+            const {user, token, location} =  payload;
+            state.status = STATUS.LOADED;
+            state.user = user;
+            state.token = token;
+            addUserLocalStorage({user, token, location})
+        })
+        builder.addCase(registerThunk.rejected, (state) => {
+            state.status = STATUS.ERROR
+        })
+
+        //login
+        builder.addCase(loginThunk.pending, (state) => {
+            state.status = STATUS.LOADING
+            state.user = null;
+        })
+        builder.addCase(loginThunk.fulfilled, (state, {payload}) => {
+            const {user, token, location} =  payload;
+            state.status = STATUS.LOADED;
+            state.user = user;
+            state.token = token;
+            addUserLocalStorage({user, token, location})
+        })
+        builder.addCase(loginThunk.rejected, (state) => {
+            state.status = STATUS.ERROR
+        })
+    })
 })
 
-export const {addUserTest} = auth.actions
+export const {} = auth.actions
 export default auth.reducer
