f4fef3005c49b9b47352954ec6f4b6fcca754a0e
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-17 09:38:03 +0300

    toastContainer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Dashboard, Main, Error404, Login, Register} from "./pages";
 import {AuthProvider, UserProvider} from "./hooks/AuthProvider";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 function App() {
   return (
      <BrowserRouter>
+         <ToastContainer
+             position="top-right"
+             autoClose={4000}
+             hideProgressBar={false}
+             newestOnTop={false}
+             closeOnClick
+             rtl={false}
+             pauseOnFocusLoss
+             draggable
+             pauseOnHover
+         />
          <Routes>
              <Route  element={<UserProvider/>}>
                  <Route path='/login' element={<Login/>}/>
@@ -18,6 +31,7 @@ function App() {
              </Route>
              <Route path='*' element={<Error404/>}/>
          </Routes>
+
      </BrowserRouter>
   );
 }
