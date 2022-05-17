5777c65f9eb06e8429cd3c1236858155cd09e285
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-17 09:25:27 +0300

    add error handler

--- backend/middleware/error-handler.ts
+++ backend/middleware/error-handler.ts
@@ -1,11 +1,27 @@
-import { Response, Request, NextFunction } from 'express'
+import {Response, Request, NextFunction, ErrorRequestHandler} from 'express'
+import {Error } from 'mongoose';
+import {MongoError} from 'mongodb';
+import {StatusCodes} from 'http-status-codes'
 
-interface Error {
-    status?: number;
-    message?: string;
+interface CustomError extends Error {
+    statusCode?: number;
 }
 
-export const errorHandlerMiddleware = (err : Error, req: Request, res: Response, next: NextFunction) => {
-    console.log(err)
-    res.status(500).json({message: 'there was an error'})
+export const errorHandlerMiddleware = (error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    const defaultError = {
+        statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: error.message || 'Something went wrong, try again later'
+    }
+    if (error instanceof Error.ValidationError) {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.msg = Object.values(error.errors)
+            .map((item: any) => item.message)
+            .join(',')
+    }
+    if ((error as MongoError).code === 11000) {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        //@ts-ignore
+        defaultError.msg = `${Object.keys(error.keyValue)} field has to be unique`
+    }
+    res.status(defaultError.statusCode).json({msg: defaultError.msg})
 }
