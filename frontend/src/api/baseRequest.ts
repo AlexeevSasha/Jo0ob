2327fd49a7e5bc11f5eddde036f94d70446302cf
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-17 09:34:54 +0300

    base request

---
+++ frontend/src/api/baseRequest.ts
@@ -0,0 +1,50 @@
+import {errorNotification} from "../hooks/notification";
+
+export const BASE_URL = 'http://localhost:5000'
+
+const request = async (url: string, data: any, token: string | undefined) => {
+    const headersToken = token ? {Authorization: `Bearer ${token}`} : {}
+    const headersMultiPart = typeof data.body === 'string' ? {"Content-type": "application/json;charset=utf-8"} : {}
+
+    const response = await fetch(url, {
+        ...data,
+        headers: {
+            ...headersToken,
+            ...headersMultiPart
+        },
+    });
+
+    if (response.ok) {
+        if (response.headers.get('Content-Length') === '0') {
+            return true
+        }
+        const typeResponse = response.headers.get("Content-type");
+        let result;
+        if (typeResponse === 'aplication/text') {
+            result = await response.text()
+            return result
+        }
+        result = await response.json()
+        return result;
+    } else {
+        if (response.status === 400 ||  response.status === 401) {
+            const textJson: {msg: string} = await response.json();
+            errorNotification(textJson.msg)
+            throw new Error(textJson.msg);
+        }
+        if (response.status === 409) throw new Error('Already exists');
+        else throw  {status: response.status}
+    }
+
+}
+
+export const get = (url: string, token?: string) => request(`${BASE_URL}${url}`, {method: "GET"}, token)
+
+export const post = (url: string, body: string | FormData, token?: string) => {
+    return request(`${BASE_URL}${url}`, {method: "POST", body}, token)
+}
+export const put = (url: string, body: string, token: string) => {
+    return request(`${BASE_URL}${url}`, {method: "PUT", body}, token)
+}
+export const remove = (url: string, token: string) => request(`${BASE_URL}${url}`, {method: "DELETE"}, token)
+
