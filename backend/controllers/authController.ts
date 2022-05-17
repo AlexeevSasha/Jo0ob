0005878cad1137d1b7f84b5a0618ea5ea30064a0
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-17 09:27:45 +0300

    add register and login

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,13 +1,69 @@
-import {Request, Response} from "express";
+import {Request, Response, NextFunction} from "express";
+import {StatusCodes} from 'http-status-codes'
+import User, {IUser} from '../models/User'
+import {BadRequestError, UnauthenticatedError} from "../error";
 
-export const reqister = async (req: Request, res: Response) => {
-    res.send('user register')
+export const reqister = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {email, name, password} = req.body as IUser;
+        if (!email || !name || !password) {
+            throw new BadRequestError('Please provide all values')
+        }
+
+        const userAlreadyExist = await User.findOne({email})
+        if (userAlreadyExist) {
+            throw new BadRequestError('Email already in use')
+        }
+
+        const user = await  User.create({email, name, password})
+        const token = user.createJWT();
+
+        res.status(StatusCodes.CREATED).json({
+            user: {
+                email: user.email,
+                name: user.name,
+                lastName: user.lastName,
+                location: user.location
+            },
+            token,
+            location: user.location
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
-export const login = async (req: Request, res: Response) => {
-    res.send('user login')
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {email, password} = req.body as {email: string, password: string};
+        if (!email || !password) {
+            throw new BadRequestError('Please provide all values')
+        }
+        const user = await User.findOne({email}).select('+password')
+        if (!user) {
+            throw new UnauthenticatedError('Invalid email')
+        }
+       const isPasswordCorrect = await user.comparePassword(password)
+        if (!isPasswordCorrect) {
+            throw new UnauthenticatedError('Invalid password')
+        }
+        const token = user.createJWT()
+
+        res.status(StatusCodes.OK).json({
+            user : {
+                email: user.email,
+                name: user.name,
+                lastName: user.lastName,
+                location: user.location
+            },
+            token,
+            location: user.location})
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
     res.send('user updateUser')
+    // user.findOneAndUpdate()
 }
