98a45ced8995bf634fffe437ea5d167ed3c42d3d
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-20 16:51:21 +0300

    auth middleware

--- backend/routes/authRouter.ts
+++ backend/routes/authRouter.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import {reqister, login, updateUser} from "../controllers/authController";
+import {authMiddleware} from "../middleware/auth";
 
 const router = Router()
 
 router.route('/register').post( reqister)
 router.route('/login').post(login)
-router.route('/updateUser').patch(updateUser)
+router.route('/updateUser').patch(authMiddleware,updateUser)
 
 export default router;
