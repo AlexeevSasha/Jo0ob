f03882d396d7d7163f85484fceaf8817a3b398e2
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-20 16:54:55 +0300

    update user

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,11 +1,27 @@
 import {Request, Response, NextFunction} from "express";
 import {StatusCodes} from 'http-status-codes'
 import User, {IUser} from '../models/User'
+import { Types } from 'mongoose';
 import {BadRequestError, UnauthenticatedError} from "../error";
+
+
+
+const userObj = (user: IUser & { _id: Types.ObjectId }, token: string) => {
+    return {
+        user: {
+            id: user._id,
+            email: user.email,
+            lastName: user.lastName,
+            location: user.location,
+            name: user.name,
+        },
+        token
+    }
+}
 
 export const reqister = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {email, name, password} = req.body as IUser;
+        const {email, name, password, lastName, location} = req.body as IUser;
         if (!email || !name || !password) {
             throw new BadRequestError('Please provide all values')
         }
@@ -15,19 +31,11 @@ export const reqister = async (req: Request, res: Response, next: NextFunction) 
             throw new BadRequestError('Email already in use')
         }
 
-        const user = await  User.create({email, name, password})
+
+        const user = await User.create(req.body)
         const token = user.createJWT();
 
-        res.status(StatusCodes.CREATED).json({
-            user: {
-                email: user.email,
-                name: user.name,
-                lastName: user.lastName,
-                location: user.location
-            },
-            token,
-            location: user.location
-        })
+        res.status(StatusCodes.CREATED).json(userObj(user, token))
     } catch (error) {
         next(error)
     }
@@ -35,35 +43,44 @@ export const reqister = async (req: Request, res: Response, next: NextFunction) 
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {email, password} = req.body as {email: string, password: string};
+        const {email, password} = req.body as { email: string, password: string };
         if (!email || !password) {
             throw new BadRequestError('Please provide all values')
         }
+
         const user = await User.findOne({email}).select('+password')
         if (!user) {
             throw new UnauthenticatedError('Invalid email')
         }
-       const isPasswordCorrect = await user.comparePassword(password)
+        const isPasswordCorrect = await user.comparePassword(password)
         if (!isPasswordCorrect) {
             throw new UnauthenticatedError('Invalid password')
         }
         const token = user.createJWT()
 
-        res.status(StatusCodes.OK).json({
-            user : {
-                email: user.email,
-                name: user.name,
-                lastName: user.lastName,
-                location: user.location
-            },
-            token,
-            location: user.location})
+        res.status(StatusCodes.OK).json(userObj(user, token))
     } catch (error) {
         next(error)
     }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
-    res.send('user updateUser')
-    // user.findOneAndUpdate()
+    const {email, name, lastName, location} = req.body as IUser;
+    if (!email && !name && !lastName && !location) {
+        throw new BadRequestError('Please provide  value')
+    }
+    const user = await User.findOne({_id: req.user.userId})
+
+    if (!user) {
+        throw new BadRequestError('User does not exist')
+    }
+    if (email) user.email = email;
+    if (name) user.name = name;
+    if (lastName) user.lastName = lastName;
+    if (location) user.location = location;
+
+     await user.save();
+     const token = user.createJWT()
+
+    res.status(StatusCodes.OK).json(userObj(user, token))
 }
