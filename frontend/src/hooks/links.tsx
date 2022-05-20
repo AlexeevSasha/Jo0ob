385747d1cc963372f89b664d926d474df036f7a7
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-20 16:36:45 +0300

    links array

---
+++ frontend/src/hooks/links.tsx
@@ -0,0 +1,18 @@
+import {AreaChartOutlined, AppstoreAddOutlined, FundViewOutlined, UserOutlined} from "@ant-design/icons";
+import {ReactElement} from "react";
+
+
+interface ILinks {
+    id: number;
+    text: string;
+    path: string;
+    icon: ReactElement
+}
+
+
+export const links: ILinks[] = [
+    {id: 1, text: 'Stats', path: '/dashboard', icon: <AreaChartOutlined/>},
+    {id: 2, text: 'All jobs', path: '/all-jobs', icon: <FundViewOutlined/>},
+    {id: 3, text: 'Add job', path: '/add-job', icon: <AppstoreAddOutlined/>},
+    {id: 4, text: 'Profile', path: '/profile', icon: <UserOutlined/>},
+]
