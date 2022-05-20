d683f677bca6ee479f1f168a0c4260a3cbc2be1f
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-20 16:43:57 +0300

    update user thunk

--- frontend/src/redux/thunk/auth.ts
+++ frontend/src/redux/thunk/auth.ts
@@ -1,6 +1,7 @@
 import {createAsyncThunk} from '@reduxjs/toolkit'
-import {ILogin, IRegister, IUserServer} from "../../api/auth/authDto";
-import {authLogin, authRegister} from "../../api/auth/authService";
+import {ILogin, IRegister, IUpdateUser, IUserServer} from "../../api/auth/authDto";
+import {authLogin, authRegister, authUpdate} from "../../api/auth/authService";
+import {successNotification} from "../../utils/notification";
 
 
 export const registerThunk = createAsyncThunk<IUserServer,  {data: IRegister, cb: () => void}>(
@@ -17,6 +18,15 @@ export const loginThunk = createAsyncThunk<IUserServer,  {data: ILogin, cb: () =
     async ({data, cb}) => {
         const response : IUserServer = await authLogin(data)
         cb()
+        return response
+    }
+)
+
+export const updateUserThunk = createAsyncThunk<IUserServer,  IUpdateUser>(
+    'auth/fetchUpdateUser',
+    async (data) => {
+        const response : IUserServer = await authUpdate(data);
+        successNotification('user updated successfully')
         return response
     }
 )
