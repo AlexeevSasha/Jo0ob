98d770dcba816af54b84a734b9f66f9d0c653117
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-20 16:35:24 +0300

    layout app

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,32 @@
+import {FC, useCallback, useState} from 'react'
+import {Outlet} from "react-router-dom";
+import styled from "styled-components";
+import { Navbar, Sidebar} from "./";
+
+
+export const  Layout: FC = () => {
+    const [visible, setVisible] = useState(false)
+    const toggle = useCallback(() => setVisible(!visible), [visible])
+    const closeNav = useCallback(() => setVisible(true), [])
+    return (
+        <div>
+                <Navbar visible={visible} toggle={toggle}/>
+                <Sidebar visible={visible} closeNav={closeNav}/>
+            <ContainerOutlet pd={visible}>
+           <Outlet/>
+            </ContainerOutlet>
+        </div>
+    );
+}
+
+const Main = styled.main`
+`
+
+const ContainerOutlet = styled.main<{pd: boolean}>`
+    padding: ${({pd}) => pd ? '80px 50px 0 50px' : '80px 50px 0 250px'};
+  transition: all .3s linear;
+  @media ${({theme}) => theme.media._768} {
+    padding: 60px 16px 0;
+  }
+`
+
