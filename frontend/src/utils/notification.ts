8991c183807a99655efbad306ae6c15c0b17fc6c
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-20 16:33:10 +0300

    error and success notification

--- frontend/src/utils/notification.ts
+++ frontend/src/utils/notification.ts
@@ -12,3 +12,15 @@ export const errorNotification = (msg : string) => {
         progress: undefined,
     });
 }
+
+export const successNotification = (msg : string) => {
+    toast.success(msg, {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+}
