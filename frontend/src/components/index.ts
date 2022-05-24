20df61496862816cfdc2194189b510426c0364af
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-24 19:14:57 +0300

    export import

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,3 +2,8 @@ export {Navbar} from './Navbar';
 export {Layout} from './Layout';
 export {Sidebar} from './Sidebar';
 export {NavLinks} from './NavLinks';
+export {BtnEditDelete} from './BtnEditDelete';
+export {JobCart} from './JobCart';
+export {SearchForm} from './SearchForm';
+export {FormJob} from './FormJob'
+export {StaticCard} from './StaticCard'
