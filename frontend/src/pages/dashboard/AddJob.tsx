52ac8b0610bd529c1f3c6b2e577c8ec8537aff20
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-24 19:23:32 +0300

    add job

---
+++ frontend/src/pages/dashboard/AddJob.tsx
@@ -0,0 +1,26 @@
+import {FC} from "react";
+import {useAppDispatch} from "../../redux/reduxType";
+import {IAddJob, JobType, Status} from "../../api/job/jobDto";
+import {addJobThunk} from "../../redux/thunk/job";
+import {useNavigate} from "react-router-dom";
+import {FormJob} from '../../components'
+
+
+
+export const AddJob: FC = () => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate()
+    const submitHandler = (values : IAddJob<Status, JobType>) => {
+       dispatch(addJobThunk({
+           data: {
+               ...values,
+               status: values.status,
+               type: values.type
+           },
+           cb: () => navigate('/all-jobs')
+       }))
+    };
+    return <FormJob title='Add Job' onFinish={submitHandler}/>
+}
+
+
