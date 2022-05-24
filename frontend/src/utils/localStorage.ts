67003fd1175ef5542e131f586c499b83e53247b9
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-24 19:20:00 +0300

    get token

--- frontend/src/utils/localStorage.ts
+++ frontend/src/utils/localStorage.ts
@@ -1,6 +1,8 @@
 import {IUserServer} from "../api/auth/authDto";
 
 
+
+
 export const addUserLocalStorage = ({user, token} : IUserServer) => {
     localStorage.setItem('user', JSON.stringify(user))
     localStorage.setItem('token', token)
@@ -10,6 +12,7 @@ export const removeUserLocalStorage = () => {
     localStorage.removeItem('user')
     localStorage.removeItem('token')
     localStorage.removeItem('location')
+    window.location.replace("http://localhost:3000/");
 }
 
 export const getToken = () => localStorage.getItem("token")
