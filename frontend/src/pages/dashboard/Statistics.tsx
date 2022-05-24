94c75e086d90d5a38d7fd15d7a828043c4f45ec8
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-24 19:24:42 +0300

    add staticcart

---
+++ frontend/src/pages/dashboard/Statistics.tsx
@@ -0,0 +1,17 @@
+import {FC} from "react";
+import {Button, Typography} from 'antd';
+import styled from 'styled-components'
+import {StaticCard} from "../../components";
+
+
+
+export const Statistics: FC = () => {
+
+    return (
+        <div>
+            <StaticCard/>
+        </div>
+    )
+}
+
+
