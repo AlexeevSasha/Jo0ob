3101183df6c1605a5322dc26af121c1b1e136a73
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-24 19:16:22 +0300

    export import

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,7 +3,7 @@ export {Layout} from './Layout';
 export {Sidebar} from './Sidebar';
 export {NavLinks} from './NavLinks';
 export {BtnEditDelete} from './BtnEditDelete';
-export {JobCart} from './JobCart';
+export {JobCard} from './JobCard';
 export {SearchForm} from './SearchForm';
 export {FormJob} from './FormJob'
 export {StaticCard} from './StaticCard'
