b59a0cb322b4a76abc0f69baa93016296bebf0ff
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-26 20:45:05 +0300

    get all filter and search url

--- frontend/src/api/job/jobService.ts
+++ frontend/src/api/job/jobService.ts
@@ -1,15 +1,22 @@
-import {IAddJob, Status, JobType, IJobService, IStaticServer} from "./jobDto";
+import {IAddJob, Status, JobType, IJobService, IStaticServer, IFilter} from "./jobDto";
 import {post, get, remove, patch} from "../baseRequest";
 import {getToken} from "../../utils/localStorage";
-
 
 export const addJob = (data :  IAddJob<Status, JobType>) : Promise<IJobService[]> => {
     const token = getToken() as string;
     return post('/api/jobs', JSON.stringify(data), token)
 }
 
-export const getAllJob = () : Promise<IJobService[]> => {
-    return get('/api/jobs')
+export const getAllJob = (filter?: IFilter , search?: string) : Promise<IJobService[]> => {
+    let url = '/api/jobs'
+    if (!filter && !search) return get(url)
+    if (search) {
+        url += `?search=${search}`
+        return get(url)
+    }
+    const { sort, type, status} = filter as IFilter;
+    url += `?status=${status}&type=${type}&sort=&${sort}`
+    return get(url)
 }
 
 export const getByIdJob = (id: string) : Promise<IJobService> => {
@@ -26,7 +33,6 @@ export const editJob = (id: string, data: IAddJob<Status, JobType>) : Promise<IJ
     const token = getToken() as string;
     return patch(`/api/jobs/${id}`,JSON.stringify(data) , token)
 }
-
 
 export const getStaticJob = () : Promise<IStaticServer> => {
     const token = getToken() as string;
