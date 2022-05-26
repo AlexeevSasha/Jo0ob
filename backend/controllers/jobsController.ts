c27f8679d821b3d955f6a7f13f668a404bebac61
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-26 20:38:18 +0300

    add query params

--- backend/controllers/jobsController.ts
+++ backend/controllers/jobsController.ts
@@ -1,13 +1,39 @@
 import {Request, Response} from "express";
 import Job, {IJob} from "../models/Job";
 import {Types} from "mongoose";
+import moment from "moment";
 import {BadRequestError, NotFoundError} from "../error";
 import {StatusCodes} from "http-status-codes";
 import {checkPermissions} from "../utils/checkPermisions";
 
+interface IQuery {
+    status?: string
+    type?: string
+    position?: {$regex : string, $options: 'i'}
+}
+
+const sortJob = (str: string) =>{
+    switch (str) {
+        case 'latest' : return '-createAt'
+        case 'oldest' : return 'createAt'
+        case 'a-z' : return 'position'
+        case 'z-a' : return '-position'
+        default : return ''
+    }
+}
 
 export const getAllJob = async (req: Request<{ id: string }>, res: Response) => {
-    const allJob = await Job.find({})
+    const {status, type, sort, search} = req.query as { [key: string]: string }
+
+    const queryObj: IQuery = {}
+    if (status && status !== 'all') queryObj.status = status
+    if (type && type !== 'all') queryObj.type = type
+    if (search) {
+        queryObj.position = {$regex : search, $options: 'i'}
+    }
+    const sorts = sortJob(sort)
+
+    const allJob = await Job.find(queryObj).sort(sorts);
     res.status(StatusCodes.CREATED).json(allJob)
 }
 export const getByIdJob = async (req: Request, res: Response) => {
@@ -66,17 +92,32 @@ export const showStats = async (req: Request, res: Response) => {
         {$match: {createBy: new Types.ObjectId(req.user.userId)}},
         {$group: {_id: '$status', count: {$sum: 1}}}
     ])
+
+    const monthlyApplications = await Job.aggregate([
+        {$match: {createBy: new Types.ObjectId(req.user.userId)}},
+        {$group: {
+            _id: {year: {$year: '$createdAt'},
+                month: {$month: '$createdAt'},
+                count: {$sum: 1}
+            }}},
+        {$sort: {'_id.year' : -1, '_id.month': -1}},
+        {$limit: 6}
+    ])
+
     const statusObj = stats.reduce((acc, curr) => {
         acc[curr._id] = curr.count || 0
         return acc;
-    }, {})
+    }, {
+        "declined": 0,
+        "pending": 0,
+        "interview": 0
+    })
+    const monthly = monthlyApplications.map(elem => {
+        const {_id: {year, moth, count}} = elem;
+        const date = moment().month(moth - 1).year(year).format('MMM Y')
+        return {date, count}
+    })
 
-    const defaultStats = {
-        "declined": statusObj.declined || 0,
-        "pending": statusObj.pending || 0,
-        "interview": statusObj.interview || 0
-    }
-
-    res.status(StatusCodes.OK).json(defaultStats)
+    res.status(StatusCodes.OK).json({stats: statusObj,  monthly})
 }
 
