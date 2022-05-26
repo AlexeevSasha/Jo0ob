70c459cb29721d8382418f56c27103cb84395007
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-26 20:47:34 +0300

    btn loading

--- frontend/src/components/FormJob.tsx
+++ frontend/src/components/FormJob.tsx
@@ -1,17 +1,19 @@
-import {FC} from "react";
+import {FC, memo} from "react";
 import {Button, Form, Input, Select} from 'antd';
 import styled from 'styled-components'
 import {jobType, statusOption} from "../utils/selectOption";
 import {IAddJob, IJobService, JobType, Status} from "../api/job/jobDto";
+import {useAppSelector, STATUS} from "../redux/reduxType";
 
 
 interface IProps {
     title: string;
-    onFinish: (values : IAddJob<Status, JobType>) => void;
+    onFinish: (values: IAddJob<Status, JobType>) => void;
     defaultValue?: IJobService;
 }
 
-export const FormJob: FC<IProps> = ({title, onFinish, defaultValue}) => {
+export const FormJob: FC<IProps> = memo(({title, onFinish, defaultValue}) => {
+    const {status} = useAppSelector(state => state.jobs)
     return (
         <Wrapper>
             <h2>{title}</h2>
@@ -27,7 +29,10 @@ export const FormJob: FC<IProps> = ({title, onFinish, defaultValue}) => {
                     label="Position"
                     name="position"
                     initialValue={defaultValue?.position}
-                    rules={[{required: true, message: 'Please input position!'},{max: 30, message: 'Maximum 3 characters!'}]}
+                    rules={[{required: true, message: 'Please input position!'}, {
+                        max: 30,
+                        message: 'Maximum 3 characters!'
+                    }]}
                 >
                     <Input/>
                 </Form.Item>
@@ -36,7 +41,10 @@ export const FormJob: FC<IProps> = ({title, onFinish, defaultValue}) => {
                     label="Company"
                     name="company"
                     initialValue={defaultValue?.company}
-                    rules={[{required: true, message: 'Please input company!'},{max: 30, message: 'Maximum 3 characters!'}]}
+                    rules={[{required: true, message: 'Please input company!'}, {
+                        max: 30,
+                        message: 'Maximum 3 characters!'
+                    }]}
                 >
                     <Input/>
                 </Form.Item>
@@ -51,31 +59,29 @@ export const FormJob: FC<IProps> = ({title, onFinish, defaultValue}) => {
                 </Form.Item>
 
 
-
-                <Form.Item label="Status" name='status' initialValue={defaultValue?.status ? defaultValue.status : statusOption[0].value}>
-                    <Select >
-                        {statusOption.map(({value, label}, idx) =>  (
-                            <Select.Option key={idx} value={value}>{label}</Select.Option>
-                        ))}
-                    </Select>
-                </Form.Item>
-                <Form.Item label="Type" name='type' initialValue={defaultValue?.type ? defaultValue.type  : jobType[0].value}>
+                <Form.Item label="Status" name='status'
+                           initialValue={defaultValue?.status ? defaultValue.status : statusOption[0].value}>
                     <Select>
-                        {jobType.map(({value, label}, idx) =>  (
+                        {statusOption.map(({value, label}, idx) => (
                             <Select.Option key={idx} value={value}>{label}</Select.Option>
                         ))}
                     </Select>
                 </Form.Item>
-
-
-
-                <Button type="primary" htmlType="submit">
+                <Form.Item label="Type" name='type'
+                           initialValue={defaultValue?.type ? defaultValue.type : jobType[0].value}>
+                    <Select>
+                        {jobType.map(({value, label}, idx) => (
+                            <Select.Option key={idx} value={value}>{label}</Select.Option>
+                        ))}
+                    </Select>
+                </Form.Item>
+                <Button type="primary" htmlType="submit" loading={status === STATUS.LOADING}>
                     Submit
                 </Button>
             </Form>
         </Wrapper>
     )
-}
+})
 
 const Wrapper = styled.div`
   //max-width: 1200px;
@@ -85,16 +91,17 @@ const Wrapper = styled.div`
   border-radius: 10px;
   padding: 20px;
   box-shadow: 0 0 8px 0 rgba(34, 60, 80, 0.2);
-  
+
   & > form > button {
-   width: 100%;
+    width: 100%;
   }
-  
+
   & > h2 {
     position: relative;
     color: inherit;
     font-size: 50px;
     margin-bottom: 40px;
+
     &:after {
       position: absolute;
       content: '';
@@ -105,13 +112,14 @@ const Wrapper = styled.div`
       background: rgb(0 0 0 / 19%);
     }
   }
+
   @media ${({theme}) => theme.media._768} {
     width: 100%;
     & > h2 {
       font-size: 40px;
     }
   }
-  
+
 `
 
 
