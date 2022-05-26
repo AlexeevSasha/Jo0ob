5884babd62390dbf3e39e9c301e13fb2769e51d2
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-26 20:48:27 +0300

    layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,29 +1,27 @@
 import {FC, useCallback, useState} from 'react'
 import {Outlet} from "react-router-dom";
 import styled from "styled-components";
-import { Navbar, Sidebar} from "./";
+import {Navbar, Sidebar} from "./";
 
 
-export const  Layout: FC = () => {
+export const Layout: FC = () => {
     const [visible, setVisible] = useState(false)
     const toggle = useCallback(() => setVisible(!visible), [visible])
     const closeNav = useCallback(() => setVisible(true), [])
     return (
         <div>
-                <Navbar visible={visible} toggle={toggle}/>
-                <Sidebar visible={visible} closeNav={closeNav}/>
+            <Navbar visible={visible} toggle={toggle}/>
+            <Sidebar visible={visible} closeNav={closeNav}/>
             <ContainerOutlet pd={visible}>
-           <Outlet/>
+                <Outlet/>
             </ContainerOutlet>
         </div>
     );
 }
 
-const Main = styled.main`
-`
 
-const ContainerOutlet = styled.main<{pd: boolean}>`
-    padding: ${({pd}) => pd ? '80px 50px 0 50px' : '80px 50px 0 250px'};
+const ContainerOutlet = styled.main<{ pd: boolean }>`
+  padding: ${({pd}) => pd ? '80px 50px 0 50px' : '80px 50px 0 250px'};
   transition: all .3s linear;
   @media ${({theme}) => theme.media._768} {
     padding: 60px 16px 0;
