651f0208366083758c3402dafb2a111104d45852
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-27 10:03:11 +0300

    add dispatch static all job

--- frontend/src/pages/dashboard/Statistics.tsx
+++ frontend/src/pages/dashboard/Statistics.tsx
@@ -1,9 +1,15 @@
-import {FC} from "react";
+import {FC, useEffect} from "react";
 import {StaticCard, Chart} from "../../components";
+import {staticAllJobThunk} from "../../redux/thunk/job";
+import {useAppDispatch} from "../../redux/reduxType";
 
 
 export const Statistics: FC = () => {
-
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        console.log(1)
+        dispatch(staticAllJobThunk())
+    }, [])
     return (
         <div>
             <StaticCard/>
